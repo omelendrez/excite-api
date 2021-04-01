9ac7192c0369bc730c1adee2fee4f11ee46a9937
omelendrez <omar.melendrez@gmail.com>
2021-03-31 21:06:24 -0300

    fix bugs detected by DeepScan

--- app/helpers.js
+++ app/helpers.js
@@ -1,98 +1,101 @@
-const fs = require('fs')
-const fields = require('./models/db/fields.json')
-const sql = require('./models/db/db')
+const fields = require("./models/db/fields.json");
+const sql = require("./models/db/db");
 
-exports.isField = field => {
-  return fields.filter(field => field.type !== 'IGNORE').find(fld => fld.name === field)
-}
+exports.isField = (field) => {
+  return fields
+    .filter((field) => field.type !== "IGNORE")
+    .find((fld) => fld.name === field);
+};
 
-exports.formatCreateField = fieldName => {
-  if (fieldName === 'ID') {
-    return `${fieldName} INTEGER NOT NULL AUTO_INCREMENT`
+exports.formatCreateField = (fieldName) => {
+  if (fieldName === "ID") {
+    return `${fieldName} INTEGER NOT NULL AUTO_INCREMENT`;
   }
 
-  const field = fields.find(fld => fld.name === fieldName)
-  const result = [field.name]
+  const field = fields.find((fld) => fld.name === fieldName);
+  const result = [field.name];
   if (field.type) {
-    result.push(field.type)
+    result.push(field.type);
   }
   if (field.size) {
-    result.push(`(${field.size})`)
+    result.push(`(${field.size})`);
   }
   if (field.null) {
-    result.push(field.null)
+    result.push(field.null);
   }
   if (field.default !== null) {
-    result.push(`DEFAULT ${field.default}`)
+    result.push(`DEFAULT ${field.default}`);
   }
-  return result.join(' ')
-}
+  return result.join(" ");
+};
 
 exports.addIndex = (fieldName, indexes) => {
-  const field = fields.find(fld => fld.name === fieldName)
+  const field = fields.find((fld) => fld.name === fieldName);
   if (field.index) {
-    indexes.push(`INDEX (${fieldName})`)
+    indexes.push(`INDEX (${fieldName})`);
   }
-  return indexes
-}
+  return indexes;
+};
 
-exports.formatField = field => {
-
-  let { name, value } = field
-  value = value || ''
-  value = value !== 'NaN' ? value : ''
+exports.formatField = (field) => {
+  let { name, value } = field;
+  value = value || "";
+  value = value !== "NaN" ? value : "";
   value = value
-    .replace(/�/g, '')
-    .replace(/�/g, '?')
-    .replace(/\u0000/g, '')
-    .replace(/\u0003/g, '')
-    .replace(/\u0004/g, '')
-    .replace(/"/g, '')
-    .replace(/\\/g, '')
+    .replace(/�/g, "")
+    .replace(/�/g, "?")
+    .replace(/\u0000/g, "")
+    .replace(/\u0003/g, "")
+    .replace(/\u0004/g, "")
+    .replace(/"/g, "")
+    .replace(/\\/g, "");
 
-  const fieldObject = fields.find(fld => fld.name === name)
-  let result = ''
+  const fieldObject = fields.find((fld) => fld.name === name);
+  let result = "";
   switch (fieldObject.type) {
-    case 'DATE':
-      result = value ? `"${value.substr(0, 4)}-${value.substr(4, 2)}-${value.substr(6, 2)}"` : 'NULL'
-      break
-    case 'INTEGER':
-      result = parseInt(value) || 0
-      break
-    case 'DECIMAL':
-      result = parseFloat(value) || 0
-      break
+    case "DATE":
+      result = value
+        ? `"${value.substr(0, 4)}-${value.substr(4, 2)}-${value.substr(6, 2)}"`
+        : "NULL";
+      break;
+    case "INTEGER":
+      result = parseInt(value) || 0;
+      break;
+    case "DECIMAL":
+      result = parseFloat(value) || 0;
+      break;
     default:
-      result = `"${value}"`
+      result = `"${value}"`;
   }
 
-  return result
-
-}
+  return result;
+};
 
 exports.findNumber = (code, result) => {
   sql.query(`SELECT * FROM numeros WHERE NUMCOD = ${code}`, (err, res) => {
     if (err) {
-      console.log("error: ", err)
-      result(err, null)
-      return
+      console.log("error: ", err);
+      result(err, null);
+      return;
     }
 
     if (res.length) {
-      console.log("found record: ", res[0])
-      result(null, res[0])
-      return
+      console.log("found record: ", res[0]);
+      result(null, res[0]);
+      return;
     }
 
-    result({ kind: "not_found" }, null)
-  })
-}
+    result({ kind: "not_found" }, null);
+  });
+};
 
 exports.updateNumber = (code, value) => {
-  sql.query(`UPDATE numeros SET NUMVAL = ${value} WHERE NUMCOD = ${code}`, (err, res) => {
-    if (err) {
-      console.log("error: ", err)
+  sql.query(
+    `UPDATE numeros SET NUMVAL = ${value} WHERE NUMCOD = ${code}`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+      }
     }
-  })
-}
-
+  );
+};
--- app/models/model.js
+++ app/models/model.js
@@ -1,153 +1,158 @@
-const sql = require("./db/db")
-const { findNumber, updateNumber } = require("../helpers")
-const numeros = require('./db/numeros.json')
-const sqlQueries = require('./db/sql.json')
+const sql = require("./db/db");
+const { findNumber, updateNumber } = require("../helpers");
+const numeros = require("./db/numeros.json");
+const sqlQueries = require("./db/sql.json");
 
 const Model = function (record) {
-  const keys = Object.keys(record)
-  keys.map(key => this[key] = record[key])
-}
+  const keys = Object.keys(record);
+  keys.map((key) => (this[key] = record[key]));
+};
 
 Model.create = (newRecord, result, model) => {
-  const numero = numeros.find(numero => numero.model === model)
+  const numero = numeros.find((numero) => numero.model === model);
   if (numero) {
     findNumber(numero.NUMCOD, (err, data) => {
-      const value = data.NUMVAL + 1
-      newRecord[numero.field] = value
+      const value = data.NUMVAL + 1;
+      newRecord[numero.field] = value;
       sql.query(`INSERT INTO ${model} SET ?`, newRecord, (err, res) => {
         if (err) {
-          console.log("error: ", err)
-          result(err, null)
-          return
+          console.log("error: ", err);
+          result(err, null);
+          return;
         }
-        updateNumber(numero.NUMCOD, value)
-        result(null, { id: res.insertId, ...newRecord })
-      })
-    })
+        updateNumber(numero.NUMCOD, value);
+        result(null, { id: res.insertId, ...newRecord });
+      });
+    });
   } else {
     sql.query(`INSERT INTO ${model} SET ?`, newRecord, (err, res) => {
       if (err) {
-        console.log("error: ", err)
-        result(err, null)
-        return
+        console.log("error: ", err);
+        result(err, null);
+        return;
       }
-      result(null, { id: res.insertId, ...newRecord })
-    })
+      result(null, { id: res.insertId, ...newRecord });
+    });
   }
-}
+};
 
 Model.findById = (id, result, model) => {
-  const sqlQuery = `SELECT * FROM ${model} WHERE ID = ${id}`
+  const sqlQuery = `SELECT * FROM ${model} WHERE ID = ${id}`;
   sql.query(sqlQuery, (err, res) => {
     if (err) {
-      console.log("error: ", err)
-      result(err, null)
-      return
+      console.log("error: ", err);
+      result(err, null);
+      return;
     }
 
     if (res.length) {
-      result(null, res[0])
-      return
+      result(null, res[0]);
+      return;
     }
 
-    result({ kind: "not_found" }, null)
-  })
-}
+    result({ kind: "not_found" }, null);
+  });
+};
 
 Model.getAll = (query, result, model) => {
-  let { search } = query
-  let sqlQuery = ''
-  let sqlWhere = ''
+  let { search } = query;
+  let sqlQuery = "";
+  let sqlWhere = "";
 
-  const sqlObject = sqlQueries.find(query => query.model === model)
+  const sqlObject = sqlQueries.find((query) => query.model === model);
   if (sqlObject) {
     if (search) {
       if (sqlObject.where) {
-        sqlWhere = (sqlObject.all.includes('WHERE') ? 'AND ' : 'WHERE ') + `${sqlObject.where} LIKE "%${search}%"`
+        sqlWhere =
+          (sqlObject.all.includes("WHERE") ? "AND " : "WHERE ") +
+          `${sqlObject.where} LIKE "%${search}%"`;
       }
     }
-    sqlQuery = sqlObject.all.split('{search}').join(sqlWhere)
+    sqlQuery = sqlObject.all.split("{search}").join(sqlWhere);
   }
   sql.query(sqlQuery, (err, res) => {
     if (err) {
-      console.log("error: ", err)
-      result(err, null)
-      return
+      console.log("error: ", err);
+      result(err, null);
+      return;
     }
 
-    result(null, res)
-  })
-}
+    result(null, res);
+  });
+};
 
 Model.updateById = (id, record, result, model) => {
   if (record.AJUFEC) {
-    record.AJUFEC = record.AJUFEC.split('T')[0]
+    record.AJUFEC = record.AJUFEC.split("T")[0];
   }
   if (record.CLISALFEC) {
-    record.CLISALFEC = record.CLISALFEC.split('T')[0]
+    record.CLISALFEC = record.CLISALFEC.split("T")[0];
   }
   if (record.CONFEC) {
-    record.CONFEC = record.CONFEC.split('T')[0]
+    record.CONFEC = record.CONFEC.split("T")[0];
   }
   if (record.IVAFEC) {
-    record.IVAFEC = record.IVAFEC.split('T')[0]
+    record.IVAFEC = record.IVAFEC.split("T")[0];
   }
 
-  const fields = []
-  const values = []
-  Object.keys(record).filter(field => field != 'ID').map(field => {
-    fields.push(`${field} = ?`)
-    values.push(record[field])
-  })
-  values.push(id)
-  sql.query(`UPDATE ${model} SET ${fields.join(',')}  WHERE ID = ?`,
+  const fields = [];
+  const values = [];
+  Object.keys(record)
+    .filter((field) => field != "ID")
+    .forEach((field) => {
+      fields.push(`${field} = ?`);
+      values.push(record[field]);
+    });
+  values.push(id);
+  sql.query(
+    `UPDATE ${model} SET ${fields.join(",")}  WHERE ID = ?`,
     values,
     (err, res) => {
       if (err) {
-        console.log("error: ", err)
-        result(err, null)
-        return
+        console.log("error: ", err);
+        result(err, null);
+        return;
       }
 
       if (res.affectedRows == 0) {
-        result({ kind: "not_found" }, null)
-        return
+        result({ kind: "not_found" }, null);
+        return;
       }
 
-      result(null, { id: id, ...record })
+      result(null, { id: id, ...record });
     }
-  )
-}
+  );
+};
 
 Model.remove = (id, result, model) => {
-  const sqlQuery = `DELETE FROM ${model} WHERE ID = ?`
+  const sqlQuery = `DELETE FROM ${model} WHERE ID = ?`;
 
   sql.query(sqlQuery, id, (err, res) => {
     if (err) {
-      console.log("error: ", err)
-      result(err, null)
-      return
+      console.log("error: ", err);
+      result(err, null);
+      return;
     }
 
     if (res.affectedRows == 0) {
-      result({ kind: "not_found" }, null)
-      return
+      result({ kind: "not_found" }, null);
+      return;
     }
 
-    result(null, res)
-  })
-}
+    result(null, res);
+  });
+};
 
 Model.removeAll = (result, model) => {
   sql.query(`DELETE FROM ${model}`, (err, res) => {
     if (err) {
-      console.log("error: ", err)
-      result(err, null)
-      return
+      console.log("error: ", err);
+      result(err, null);
+      return;
     }
 
-    result(null, res)
-  })
-}
+    result(null, res);
+  });
+};
 
-module.exports = Model
+module.exports = Model;
