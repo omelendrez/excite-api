b104c27c00d1159dbade025714259680c0a9cac8
omelendrez <omar.melendrez@gmail.com>
2021-04-10 16:39:14 -0300

    Create active queries

--- app/routes/vendedor.routes.js
+++ app/routes/vendedor.routes.js
@@ -1,17 +1,19 @@
-module.exports = app => {
-  const controller = require("../controllers/controller.js")
-  const model = 'vendedor'
-  const url = 'vendedores'
+module.exports = (app) => {
+  const controller = require("../controllers/controller.js");
+  const model = "vendedor";
+  const url = "vendedores";
 
-  app.post(`/${url}`, controller.create(model))
+  app.post(`/${url}`, controller.create(model));
 
-  app.get(`/${url}`, controller.findAll(model))
+  app.get(`/${url}`, controller.findAll(model));
 
-  app.get(`/${url}/:id`, controller.findOne(model))
+  app.get(`/${url}-activos`, controller.findAll(`${model}-activos`));
 
-  app.put(`/${url}/:id`, controller.update(model))
+  app.get(`/${url}/:id`, controller.findOne(model));
 
-  app.delete(`/${url}/:id`, controller.delete(model))
+  app.put(`/${url}/:id`, controller.update(model));
 
-  app.delete(`/${url}`, controller.deleteAll(model))
+  app.delete(`/${url}/:id`, controller.delete(model));
+
+  app.delete(`/${url}`, controller.deleteAll(model));
 };
