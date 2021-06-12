8ef650bbf0ac0b47153794f38ae79e68a708df7b
omelendrez <omar.melendrez@gmail.com>
2021-06-12 15:09:04 -0300

    Add pagos/remitos and pagos/valores endpoints

--- app/routes/pagos.routes.js
+++ app/routes/pagos.routes.js
@@ -1,17 +1,26 @@
-module.exports = app => {
-  const controller = require("../controllers/controller.js")
-  const model = 'pagos'
-  const url = 'pagos'
+module.exports = (app) => {
+  const controller = require("../controllers/controller.js");
+  const model = "pagos";
+  const url = "pagos";
 
-  app.post(`/${url}`, controller.create(model))
+  app.post(`/${url}`, controller.create(model));
 
-  app.get(`/${url}`, controller.findAll(model))
+  app.get(`/${url}`, controller.findAll(model));
 
-  app.get(`/${url}/:id`, controller.findOne(model))
+  app.get(`/${url}/:id`, controller.findOne(model));
 
-  app.put(`/${url}/:id`, controller.update(model))
+  app.put(`/${url}/:id`, controller.update(model));
 
-  app.delete(`/${url}/:id`, controller.delete(model))
+  app.delete(`/${url}/:id`, controller.delete(model));
 
-  app.delete(`/${url}`, controller.deleteAll(model))
+  app.delete(`/${url}`, controller.deleteAll(model));
+
+  app.get(
+    `/${url}/remitos/:id`,
+    controller.findAllByParentId(`${model}-remitos`)
+  );
+  app.get(
+    `/${url}/valores/:id`,
+    controller.findAllByParentId(`${model}-valores`)
+  );
 };
