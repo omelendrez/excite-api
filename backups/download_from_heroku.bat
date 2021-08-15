3c5d830586f17a56568b4356b96c1d31c0160b08
omelendrez <omar.melendrez@gmail.com>
2021-08-15 10:36:11 -0300

    Update database cloners

--- backups/download_from_heroku.bat
+++ backups/download_from_heroku.bat
@@ -1,4 +1,4 @@
 @echo off
-set file_name=heroku_%DATE:~6,4%%DATE:~0,2%%DATE:~3,2%.sql
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -u %EXCITE_USER% --routines --column-statistics=0 -h %EXCITE_SERVER% --password=%EXCITE_PASSWORD% --databases %EXCITE_DATABASE% > %file_name%
+set file_name=%EXCITE_DATABASE%_%DATE:~6,4%%DATE:~0,2%%DATE:~3,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%.sql
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -h %EXCITE_SERVER% -u %EXCITE_USER% --routines --column-statistics=0 --password=%EXCITE_PASSWORD% --databases %EXCITE_DATABASE% > %file_name%
 pause
--- backups/download_from_local.bat
+++ backups/download_from_local.bat
@@ -1,2 +1,4 @@
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -u root --routines --column-statistics=0 --no-create-db -h 127.0.0.1 -p excite > local.sql
-@pause
+@echo off
+set file_name=local_excite_%DATE:~6,4%%DATE:~0,2%%DATE:~3,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%.sql
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -h 127.0.0.1  -u %ROOT_USER% --routines --column-statistics=0 --password=%ROOT_PASSWORD% --databases excite > %file_name%
+pause
