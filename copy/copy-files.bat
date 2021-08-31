9d9b287c92dba769e8f6629399dd350711406c83
omelendrez <omar.melendrez@gmail.com>
2021-08-31 08:48:28 -0300

    Add path to file copier batch

--- copy/copy-files.bat
+++ copy/copy-files.bat
@@ -1,6 +1,7 @@
 @echo off
 set src_folder=C:\$sistemaNuevo
+rem set src_folder=C:\Users\Omar\Documents\excite\$sistemaNuevo
 set dst_folder=*.*
 for /f "tokens=*" %%i in (files-list.txt) DO (
-    xcopy "%src_folder%\%%i" "%dst_folder%"
+    xcopy "%src_folder%\%%i.*" "%dst_folder%"
 )
