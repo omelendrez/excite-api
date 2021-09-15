c3ae76759d328dda8ccb7a017e0868e4fbdf4360
omelendrez <omar.melendrez@gmail.com>
2021-09-15 19:59:01 -0300

    Remove node_modules, package-lock.json and run 'npm i'

--- app/models/db/db.js
+++ app/models/db/db.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  connetionLimit: 10,
+  // connetionLimit: 10,
   host: process.env.SQL_SERVER,
   database: process.env.SQL_DATABASE,
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
   multipleStatements: true,
-  timezone: "UTC",
+  timezone: "+00:00",
   dateStrings: ["DATE", "DATETIME"],
 });
 
