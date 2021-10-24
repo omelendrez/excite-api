2d59165ebde78cc27f56ee680980a3095f22673f
omelendrez <omar.melendrez@gmail.com>
2021-10-24 16:43:33 -0300

    Filter products by subtipo by date

--- app/routes/producto.routes.js
+++ app/routes/producto.routes.js
@@ -1,34 +1,34 @@
 module.exports = (app) => {
-  const controller = require("../controllers/controller.js");
-  const model = "producto";
-  const url = "productos";
+  const controller = require("../controllers/controller.js")
+  const model = "producto"
+  const url = "productos"
 
-  app.post(`/${url}`, controller.create(model));
+  app.post(`/${url}`, controller.create(model))
 
-  app.get(`/${url}`, controller.findAll(model));
+  app.get(`/${url}`, controller.findAll(model))
 
-  app.get(`/${url}-activos`, controller.findAll(`${model}-activos`));
+  app.get(`/${url}-activos`, controller.findAll(`${model}-activos`))
 
   app.get(
     `/${url}-ajustes/:id`,
     controller.findAllByParentId(`${model}-ajustes`)
-  );
+  )
 
   app.get(
     `/${url}-ventas-producto/:id`,
     controller.findAllByParentId(`${model}-ventas`)
-  );
+  )
 
   app.get(
     `/${url}-ventas-subtipo`,
-    controller.findAll(`${model}-ventas-subtipo`)
-  );
+    controller.findAllByParentId(`${model}-ventas-subtipo`)
+  )
 
-  app.get(`/${url}/:id`, controller.findOne(model));
+  app.get(`/${url}/:id`, controller.findOne(model))
 
-  app.put(`/${url}/:id`, controller.update(model));
+  app.put(`/${url}/:id`, controller.update(model))
 
-  app.delete(`/${url}/:id`, controller.delete(model));
+  app.delete(`/${url}/:id`, controller.delete(model))
 
-  app.delete(`/${url}`, controller.deleteAll(model));
-};
+  app.delete(`/${url}`, controller.deleteAll(model))
+}
