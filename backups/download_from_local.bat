50bdee979358163b057bd1cdabe9b67352678321
omelendrez <omar.melendrez@gmail.com>
2021-12-01 09:13:40 -0300

    Update database

--- backups/download_from_local.bat
+++ backups/download_from_local.bat
@@ -1,4 +1,6 @@
 @echo off
-set file_name=local_excite_%DATE:~6,4%%DATE:~0,2%%DATE:~3,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%.sql
+rem set file_name=local_excite_%DATE:~6,4%%DATE:~0,2%%DATE:~3,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%.sql
+set file_name=local_excite_.sql
+echo %file_name%
 "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -h 127.0.0.1  -u %ROOT_USER% --routines --column-statistics=0 --password=%ROOT_PASSWORD% --databases excite > %file_name%
 pause
